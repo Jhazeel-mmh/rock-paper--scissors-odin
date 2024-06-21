0631a550d6ac17112a8910b0a42105c81f89d31a
Jhazeel Hernandez <jhazeelhdz@gmail.com>
2024-06-21 10:26:54 -0600

    Add the comments/algorithm to script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello world")
+
+// play game function, playRound(humanChoice, compiterChoice), getComputerChoice, getHumanChoice, score variables
+
+// optons == minus
+//create scoreHumnan and scoreComputer
+
+// for loop to repeat the game for 5 rounds
+// create getComputerChoice and store it in computerChoice, if 1 = rock, if else 2 = paper, else if 3 = scissors, 
+
+// create getHumanChoice and store it in humanChoice, case insensitive and strip the blank, loop intil the user enters a valid input, return user input
+
+// create playRound that takes human and computer choices and  compare their options 
+// create a dict name options with option: the option  that defeats that option
+// if options[humnaChoice] === computerChoice:
+        // computer wins and add the score
+//  else if options[computerChoice] === humanChoice
+//         human wins and add the score
+//  else is a tie
+
